72c7c1ab7b1810ed40cbe4cf1cff933a436c0c7b
clovis <clovisgladstone@gmail.com>
2012-06-03 23:00:43 -0500

    remove unneeded function

--- reports/get_text.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-def get_text(hit, byte_start, length, path):
-    file_path = path + '/data/TEXT/' + hit.filename
-    file = open(file_path)        
-    file.seek(byte_start)
-    return file.read(length)
