9e3a952514a8025725f12e75bcdac804453c361f
Richard Whaling <rwhaling@robespierre.ci.uchicago.edu>
2012-06-20 12:48:24 -0500

    added pages to conc cite, modified directory root URL handling in dispatcher and htaccess

--- dispatcher.py
+++ dispatcher.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import urlparse
 import reports
 from wsgiref.handlers import CGIHandler
@@ -8,14 +9,19 @@ from cgi import FieldStorage
 
 def philo_dispatcher(environ,start_response):
     report = FieldStorage().getvalue('report')
-    path_components = [c for c in environ["PATH_INFO"].split("/") if c]
+    try:
+        path_components = [c for c in environ["PATH_INFO"].split("/") if c]
+    except:
+        path_components = []
     if path_components:
         if path_components[0] == "form":
             yield reports.form(start_response, environ)
         else:
             yield getattr(reports, report or "navigation")(start_response, environ)
-    else:
+    elif environ["QUERY_STRING"]:
         yield getattr(reports, report or "concordance")(start_response, environ)
+    else:
+        yield reports.form(start_response,environ)
         
 if __name__ == "__main__":
     CGIHandler().run(philo_dispatcher)
