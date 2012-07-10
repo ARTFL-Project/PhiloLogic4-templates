abf27893ef0e9b3aab22456a1132ce9f7574db20
Clovis Gladstone <clovis@lafayette.uchicago.edu>
2012-07-10 10:12:58 -0500

    use same code for all frequency requests

--- scripts/get_frequency.py
+++ scripts/get_frequency.py
@@ -20,24 +20,7 @@ def generate_link(field, metadata_field, query):
     link = '<a href="%s">' % url + field + '</a>'
     return link
 
-def get_frequency_for_one_term(query, metadata_field, db):
-    c = db.dbh.cursor()
-    query = query.decode('utf-8', 'ignore')
-    sql_query = 'select philo_id, doc_token_count from doc_word_counts where philo_name=? order by doc_token_count desc'
-    c.execute(sql_query, (query,))
-    results = {}
-    for i in c.fetchall():
-        sql_query = 'select %s from toms where philo_id=?' % metadata_field
-        c.execute(sql_query, (i[0],))
-        field = c.fetchone()[0].decode('utf-8', 'ignore')
-        link = generate_link(field, metadata_field, query) 
-        if link not in results:
-            results[link] = 0 
-        results[link] += int(i[1])
-    results = sorted(results.iteritems(), key=lambda x: x[1], reverse=True)[:50]
-    return json.dumps(results)
-    
-def get_frequency_for_multiple_terms(query, metadata_field, db):
+def get_frequency(query, metadata_field, db):
     hits = db.query(term,'','',**{})
     q = {"field": metadata_field, "rate": ""}
     metadata_field, freq_results = r.generate_frequency(hits, q, db)
@@ -57,10 +40,7 @@ if __name__ == "__main__":
     form = cgi.FieldStorage()
     term = form.getvalue('term')
     field = form.getvalue('field')
-    if re.search('[ |]+', term):
-        results = get_frequency_for_multiple_terms(term, field, db)
-    else:
-        results = get_frequency_for_one_term(term, field, db)
+    results = get_frequency(term, field, db)
     print "Content-Type: text/html\n"
     print results
     
